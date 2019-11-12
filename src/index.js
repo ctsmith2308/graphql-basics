858a24122dec642a188d37b1f064dfda033ab63a
Christopher Smith <ctsmith2308@gmail.com>
2019-11-12 16:20:35 -0700

    query for comments success

--- src/index.js
+++ src/index.js
@@ -13,27 +13,39 @@ const posts = [
     id: "1",
     title: "What title",
     body: "Body of the post this",
-    published: true
+    published: true,
+    author: '1'
   },
   {
     id: "2",
     title: "This title",
     body: "Body of the post that",
-    published: false
+    published: false,
+    author: '1'
+
   },
   {
     id: "3",
     title: "No titles",
     body: "Body of the post what",
-    published: true
+    published: true,
+    author: '2'
   }
 ];
+
+const comments = [
+  { id: "11", text: "Chris is great" },
+  { id: "12", text: "Sara is awesome" },
+  { id: "13", text: "Rachel is annoying" }
+];
+
 
 // Type definitions (schema)
 const typeDefs = `
   type Query {
     users(query: String): [User!]!
     posts(query: String): [Post!]!
+    comments(query: String): [Comment!]!
     me: User!
     post: Post!
   }
@@ -43,6 +55,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -50,6 +63,12 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
   }
 `;
 
@@ -77,21 +96,34 @@ const resolvers = {
         );
       }
     },
-    me() {
-      return {
-        id: "123",
-        name: "Chris Smith",
-        email: "yahoo@yahoo.com",
-        age: null
-      };
-    },
-    post() {
-      return {
-        id: "345",
-        title: "First Post",
-        body: "I am the body of the post",
-        published: true
-      };
+    comments(parent, args, ctx, info) {
+      return comments
+    }
+    // me() {
+    //   return {
+    //     id: "123",
+    //     name: "Chris Smith",
+    //     email: "yahoo@yahoo.com",
+    //     age: null
+    //   };
+    // },
+    // post() {
+    //   return {
+    //     id: "345",
+    //     title: "First Post",
+    //     body: "I am the body of the post",
+    //     published: true
+    //   };
+    // }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author)
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => post.author === parent.id)
     }
   }
 };
