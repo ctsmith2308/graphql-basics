d38ac26f6d164c50a33dc64f5aef1e634d01f5d6
Christopher Smith <ctsmith2308@gmail.com>
2019-11-18 11:16:46 -0700

    moved comment resolvers to comment.js

--- src/index.js
+++ src/index.js
@@ -9,18 +9,7 @@ import db from "./db";
 const resolvers = {
   
  
-  Comment: { // In order to access the authors or posts from a query WITHIN a comment query, resolver functions for each TYPE (author and post) are needed below:
-    // Note: Parent, from what I'm loosely assuming is equal to a single Comment, specifically the Comment within Comments above.
-    // How this works is for each comment in the comments array, the Parent (a singular Comment) is passed into the author/posts resolvers 
-    // below, which allows the author/posts resolver functions to access the values of each Comment (the parent). 
-    // It is a built in GraphQL loop that queries each Comment ie Parent against the find function within the resolver below
-    author(parent, args, { db }, info) {
-      return db.users.find((user) => user.id === parent.author)
-    },
-    post(parent, args, { db }, info) {
-      return db.posts.find(post => post.id === parent.post)
-    }
-  }
+ 
 };
 
 // Intialize new GraphQLServer and pass in typeDefs and resolvers defined above.
