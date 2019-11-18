a9ccf2563e87806d9bdd09f2d3413902382937a7
Christopher Smith <ctsmith2308@gmail.com>
2019-11-18 11:15:46 -0700

    moved user resolvers to users.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,7 @@ import db from "./db";
 // You must define a corresponding resolver method for each Query or Mutation method you create. 
 const resolvers = {
   
-  User: { // In order to access the comments or posts from a query WITHIN a user query, resolver functions for each TYPE (comments and post) are needed below:
-    // Note: Parent, from what I'm loosely assuming is equal to a single User, specifically the User within Users above.
-    // How this works is for each User in the Users array, the Parent (a singular User) is passed into the posts/comments resolvers 
-    // below, which allows the posts/comments resolver functions to access the values of each User (the parent). 
-    // It is a built in GraphQL loop that queries each User ie Parent against the find function within the resolver below
-    posts(parent, args, { db }, info) {
-      return db.posts.filter(post => post.author === parent.id)
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter(comment => comment.author === parent.id)
-    }
-  },
+ 
   Comment: { // In order to access the authors or posts from a query WITHIN a comment query, resolver functions for each TYPE (author and post) are needed below:
     // Note: Parent, from what I'm loosely assuming is equal to a single Comment, specifically the Comment within Comments above.
     // How this works is for each comment in the comments array, the Parent (a singular Comment) is passed into the author/posts resolvers 
